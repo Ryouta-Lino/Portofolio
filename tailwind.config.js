dc33629addd65fe210f8dd0c3b0373106cecc18c
Lino <Dedeichsan.r15@gmail.com>
2025-02-10 17:52:23 +0700

    Porofolio

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './dist/**/*.{html,js}', // Sesuaikan dengan path file Anda
+  ],
+  theme: {
+    
+    extend: {
+         colors:{
+           primary: `#14b8a6`,
+              secondary: `#0f172a`,
+        },
+    },
+  },
+  plugins: [],
+}
